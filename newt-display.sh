e4994d7f41496ff7e1190c2b3fba497cf456b781
Keagan Winterthieme <hopefullifetwo@gmail.com>
2017-01-27 16:35:17 -0600

    Renamed ibs main script, added newt-display.sh

--- newt-display.sh
+++ newt-display.sh
@@ -15,11 +15,41 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# IBS is the main user-facing wrapper script.
+# newt-display.sh is a library designed to make using whiptail easier in bash.
 #
 # IBS is the LibraNext Integrated Build System. It's primary purpose is to
 # 	enable easier building of the LibraNix operating system and ensuring all
 #	team members are using the same options.
 
-# Paint a splash screen to welcome the user.
 
+
+# First we hve to check what terminal we are using. Whiptail/newt has a bug that prevents the --infobox option from working correctly in xterm.
+
+if [ $TERM == xterm  ]
+then
+	WHIPBUG=1
+else
+	WHIPBUG=""
+fi
+
+# Next we want to set a few functions to make drawing windows easier.
+
+
+# Function wInfoBox expects arguments in order: backtitle, title, text for box, height of dialog, width of dialog.
+function wInfoBox() {
+
+if [ -n $WHIPBUG ]
+then
+	WHIPCMD="TERM=v220 whiptail"
+else
+	WHIPCMD="whiptail"
+fi
+
+if [ -z $6 ]
+then
+	clear
+fi
+
+$WHIPCMD --backtitle $1 --title $2 --infobox $3 $4 $5
+
+}
